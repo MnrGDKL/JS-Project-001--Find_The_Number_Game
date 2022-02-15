f62d3bafe0c0ee6f6a7a1ef30920916123851ba4
MnrGDKL <munirgedikli.2021@gmail.com>
2022-02-15 16:01:29 +0300

    quess algorith has been updated

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ let max = 100;
 let count = 10;
 let userNum = 0;
 let quessNums = [];
+let difference = 0;
 
 quessBtn.addEventListener("click", ()=>{
     count--;
     userNum = +inputNumber.value;
+    difference = Math.abs(myNumber-userNum);
     if (count > 0){
         if (0 < userNum && userNum <= 100){
             userNum = +inputNumber.value;
@@ -26,16 +28,24 @@ quessBtn.addEventListener("click", ()=>{
             if (userNum !== myNumber) {
                 if (userNum < myNumber) {
                     min = userNum;
-                    quessResult.innerText = `Too Low!\nGo Up`;
+                    quessResult.innerText =  (difference <5) ? `Little Low!\nGo Up` 
+                                           : (difference <10) ? `Low!\nGo Up`
+                                           : `Too Low!\nGo Up`;
+
                     prevAttempts.innerText = `Your Previous Quesses: \n${quessNums.join(" - ")}`;
                     attemptNumber.textContent = count + " left";
                     inputNumber.value = "";
+                    inputNumber.focus();
                 } else {
                     max = userNum;
-                    quessResult.innerText = `Too High!\nGo Down`;
-                    prevAttempts.innerText = `Your Previous Quesses: \n${quessNums.join(" - ")}`;
+                    quessResult.innerText =   (difference <5) ? `Little High!\nGo Down` 
+                                            : (difference <10) ? `High\nGo Down`
+                                            : `Too High!\nGo Down`;
+
+                    prevAttempts.innerText = `Your Previous Guesses: \n${quessNums.join(" - ")}`;
                     attemptNumber.textContent = count + " left";
                     inputNumber.value = "";
+                    inputNumber.focus();
                 }
             }
         
@@ -49,12 +59,18 @@ quessBtn.addEventListener("click", ()=>{
         else{
             quessResult.innerText = `Please, Enter only a number between ${min} and ${max}.`;
             inputNumber.value = "";
+            inputNumber.focus();
             count++;
         }
     }
     else{
-        quessResult.innerText = `YOU LOSE \nWould You like To Play Again?`;
-        attemptNumber.textContent = count + " left";
+        quessResult.innerText = `YOU LOSE \n \n Would You like To Play Again?`;
+        // alert("You have no more chance to continue.");
+        Swal.fire(
+            'YOU LOSE!',
+            'Try Again',
+            'error'
+          )
     }
 });
 
